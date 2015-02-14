cdf4606dd0fee508f3121d5e964b57b9f1218378
B Chen <bchen.us@gmail.com>
2015-02-13 22:44:50 -0800

    Permutation
    Permute all possible permutation of string "abcd" in lexicographically
    increasing order

---
+++ Permutations/permute.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	string str = "abcd";
+	cout << "Welcome to c++" << endl;
+	do {
+		cout << str << endl;
+	
+	} while (next_permutation(str.begin(), str.end()));
+}
